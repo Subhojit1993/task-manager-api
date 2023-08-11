ae2d4bd3b2acbba7765f2208e8dfa86a91dfab7a
Subhojit1993 <subho.deb93@gmail.com>
2023-08-11 07:15:14 +0530

    updated user model

--- src/models/user.js
+++ src/models/user.js
@@ -79,7 +79,7 @@ userSchema.methods.generateAuthToken = async function () {
     const user = this;
 
     // pass the payload and secret
-    const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse');
+    const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
 
     user.tokens = user.tokens.concat({ token });
     await user.save();
